beb71bc7413fd268569205eb8e4fa82be3954531
Erbolat Bolatbekuly Tursynbek <erbolat_tursynbek@hotmail.com>
2022-03-11 14:59:53 +0600

    Добавьте файлы проекта.

---
+++ UploadDownload/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace UploadDownload
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(30, 25);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(108, 27);
+            this.btnUpload.TabIndex = 0;
+            this.btnUpload.Text = "Жүктеу";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(74, 106);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(635, 247);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView_CellClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnUpload);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ UploadDownload/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace UploadDownload
+{
+    public partial class Form1 : Form
+    {
+        public static string ConnectionString= @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\source\repos\UploadDownload\UploadDownload\Database1.mdf;Integrated Security=True";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BindGrid();
+            DataGridViewLinkColumn lnkDownload = new DataGridViewLinkColumn();
+            lnkDownload.UseColumnTextForLinkValue = true;
+            lnkDownload.LinkBehavior = LinkBehavior.SystemDefault;
+            lnkDownload.Name = "lnkDownload";
+            lnkDownload.HeaderText = "Жүктеу";
+            lnkDownload.Text = "Жүктеу";
+            dataGridView1.Columns.Insert(2, lnkDownload);
+            dataGridView1.CellContentClick += new DataGridViewCellEventHandler(DataGridView_CellClick);
+        }
+
+        private void DataGridView_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                int ?id=null;
+                try
+                {
+                    id = Convert.ToInt32((row.Cells[0].Value));
+                }
+                catch (InvalidCastException) {
+                    MessageBox.Show("Cіз файлды таңдамадыңыз");
+                }
+                
+                byte[] bytes;
+                string fileName, contentType;
+                using (SqlConnection con = new SqlConnection(ConnectionString))
+                {
+                    if (id != null)
+                    {
+                        using (SqlCommand cmd = new SqlCommand())
+                        {
+                            cmd.CommandText = "select Name, Data, ContentType from FileUp where Id=@Id";
+                            cmd.Parameters.AddWithValue("@Id", id);
+                            cmd.Connection = con;
+                            con.Open();
+                            using (SqlDataReader sdr = cmd.ExecuteReader())
+                            {
+                                sdr.Read();
+                                bytes = (byte[])sdr["Data"];
+                                contentType = sdr["ContentType"].ToString();
+                                fileName = sdr["Name"].ToString();
+
+                                Stream stream;
+                                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                                saveFileDialog.Filter = "All files (.*|*.)";
+                                saveFileDialog.FilterIndex = 1;
+                                saveFileDialog.RestoreDirectory = true;
+                                saveFileDialog.FileName = fileName;
+                                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                                {
+                                    stream = saveFileDialog.OpenFile();
+                                    stream.Write(bytes, 0, bytes.Length);
+                                    stream.Close();
+                                }
+                            }
+                        }
+                    }
+                    con.Close();
+                }
+            }
+        }
+        private void BindGrid()
+        {
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("select Id, Name from FileUp", con))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        using (DataTable dt = new DataTable())
+                        {
+                            sda.Fill(dt);
+                            dataGridView1.DataSource = dt;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog1 = new OpenFileDialog())
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    string fileName = openFileDialog1.FileName;
+                    byte[] bytes = File.ReadAllBytes(fileName);
+                    string contentType = "";
+                    //Файл кеңейтімі(расширение) негізінде 
+
+                    
+                    switch (Path.GetExtension(fileName))
+                    {
+                        case ".jpg":
+                            contentType = "image/jpeg";
+                            break;
+                        case ".png":
+                            contentType = "image/png";
+                            break;
+                        case ".gif":
+                            contentType = "image/gif";
+                            break;
+                        case ".bmp":
+                            contentType = "image/bmp";
+                            break;
+                            
+                    }
+
+                    using (SqlConnection conn = new SqlConnection(ConnectionString))
+                    {
+                        string sql = "INSERT INTO FileUp VALUES(@Name, @ContentType, @Data)";
+                        using (SqlCommand cmd = new SqlCommand(sql, conn))
+                        {
+                            cmd.Parameters.AddWithValue("@Name", Path.GetFileName(fileName));
+                            cmd.Parameters.AddWithValue("@ContentType", contentType);
+                            cmd.Parameters.AddWithValue("@Data", bytes);
+                            conn.Open();
+                            cmd.ExecuteNonQuery();
+                            conn.Close();
+                        }
+                    }
+                    this.BindGrid();
+                }
+            }
+        }
+    }
+}
